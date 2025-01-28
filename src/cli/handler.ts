2da7f10fa6db53dd7c89f63ef6688f7e1355bd40
Tai Porto <tai.porto@pipefy.com>
2025-01-28 19:15:57 -0300

    WIP: add progress spinner

--- src/cli/handler.ts
+++ src/cli/handler.ts
@@ -4,7 +4,7 @@ import { fitbodDataToGeneralExercises } from "../utils/parsers/fitbodToHevyExers
 import { WorkoutBuilder } from "../main/hevy/workoutBuilder";
 import { WorkoutPublisher } from "../main/hevy/workoutPublisher";
 import { save_api_key, use_api_key } from "./db";
-const cliProgress = require("cli-progress");
+import yoctoSpinner from "yocto-spinner";
 
 function initializeAPI() {
   const hevyAPIKey = use_api_key();
@@ -22,56 +22,39 @@ export async function setAPIKey(APIKey: string) {
 }
 
 export async function convert(filePath: string) {
-  const hevyAPI = initializeAPI();
-  const progressBar = new cliProgress.SingleBar(
-    {},
-    cliProgress.Presets.shades_classic
-  );
+  const spinner = yoctoSpinner({ text: "Converting exercises..." }).start();
 
-  progressBar.start(100, 0);
+  const hevyAPI = initializeAPI();
+
   const csvContent = await Bun.file(filePath).text();
-  progressBar.update(20);
 
   const exerciseData = fitbodDataToGeneralExercises(CSVtoJSON(csvContent));
-  progressBar.update(40);
 
   const workoutBuilder = new WorkoutBuilder(exerciseData, hevyAPI);
-  progressBar.update(60);
 
   const workouts = await workoutBuilder.buildWorkoutsFromGeneralExercises();
-  progressBar.update(80);
 
   Bun.file("hevy-workouts.json").write(JSON.stringify(workouts, null, 2));
-  progressBar.update(100);
-  progressBar.stop();
-  console.log(
-    "Your Fitbod exercises have been converted and saved to hevy-workouts.json"
-  );
+  spinner.success("Your workouts have been saved to hevy-workouts.json");
 }
 
 export async function publish(filePath: string) {
+  const spinner = yoctoSpinner({ text: "Publishing workout..." }).start();
   const hevyAPI = initializeAPI();
-  const progressBar = new cliProgress.SingleBar(
-    {},
-    cliProgress.Presets.shades_classic
-  );
-
   const publisher = new WorkoutPublisher(hevyAPI);
   const workout = JSON.parse(await Bun.file(filePath).text());
-  progressBar.update(50);
 
   try {
     await publisher.publishOneWorkout(workout);
-    progressBar.update(100);
-    progressBar.stop();
-    console.log("Workout has been published to your Hevy account");
+    spinner.success("Your workout has been published to your Hevy account");
   } catch (error) {
-    progressBar.stop();
-    console.error("Error publishing workout", error);
+    spinner.error("Error publishing workout");
+    console.error(error);
   }
 }
 
 export async function bulkPublish(filePath: string) {
+  const spinner = yoctoSpinner({ text: "Publishing workouts..." }).start();
   const hevyAPI = initializeAPI();
 
   const publisher = new WorkoutPublisher(hevyAPI);
@@ -79,27 +62,32 @@ export async function bulkPublish(filePath: string) {
 
   try {
     await publisher.bulkPublishWorkouts(workouts);
-    console.log("Workouts have been published to your Hevy account");
+    spinner.success("Workouts have been published to your Hevy account");
   } catch (error) {
-    console.error("Error publishing workouts", error);
+    spinner.error("Error publishing workouts");
+    console.error(error);
   }
 }
 
 export async function convertAndPublish(filePath: string) {
+  const spinner = yoctoSpinner({
+    text: "Converting exercises to Hevy workouts...",
+  }).start();
   const hevyAPI = initializeAPI();
   const csvContent = await Bun.file(filePath).text();
 
   const exerciseData = fitbodDataToGeneralExercises(CSVtoJSON(csvContent));
-
   const workoutBuilder = new WorkoutBuilder(exerciseData, hevyAPI);
   const workouts = await workoutBuilder.buildWorkoutsFromGeneralExercises();
 
+  spinner.text = "Publishing Hevy workouts...";
   const publisher = new WorkoutPublisher(hevyAPI);
 
   try {
     await publisher.bulkPublishWorkouts(workouts);
-    console.log("Workouts have been published to your Hevy account");
+    spinner.success("Workouts have been published to your Hevy account");
   } catch (error) {
-    console.error("Error publishing workouts", error);
+    spinner.error("Error publishing workouts");
+    console.error(error);
   }
 }
